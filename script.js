a9f7cab47d8dd49308f0fa0768c8100496db7089
Shikhar Verma <89732561+ShikharVerma1922@users.noreply.github.com>
2024-05-12 13:52:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -207,7 +207,8 @@ function doMouseDown(event) {
     (!gameOver &&
       G1.terminalForBigBoard(currentBigBoardState, 999) &&
       G1.terminalForBigBoard(currentBigBoardState, "D")) ||
-    !G1.action(currentSmallBoardState, currentBigBoardState, j)
+    !G1.action(currentSmallBoardState, currentBigBoardState, j) ||
+    !G1.currentLegalMoveForX(currentSmallBoardState, currentBigBoardState, j)
   ) {
     if (soundOpen) setTimeout(SOUNDS.gameEndSound(), 1000);
     updateScore();
